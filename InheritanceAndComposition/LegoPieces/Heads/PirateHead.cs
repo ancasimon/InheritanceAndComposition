9764a45d522ee6b55ee31d605dce752d92067cc0
Anca Simon <ancarsimon@gmail.com>
2020-08-10 19:07:46 -0500

    Add project files.

---
+++ InheritanceAndComposition/LegoPieces/Heads/PirateHead.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceAndComposition.LegoPieces.Heads
+{
+    class PirateHead
+    {
+        public bool HasPatch { get; set; }
+        public string FacialHair { get; set; }
+        public HairColor HairColor { get; set; }
+        public EyeColor EyeColor { get; set; }
+
+        public void Sing()
+        {
+            Console.WriteLine($"The {HairColor} pirate sang 'Yo ho ho and a bottle of run' out of tune.");
+        }
+
+        public void Laugh()
+        {
+            Console.WriteLine($"Har Har Har.");
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine("AAAAAAARGH!");
+        }
+
+    }
+
+   
+
+   
+}
